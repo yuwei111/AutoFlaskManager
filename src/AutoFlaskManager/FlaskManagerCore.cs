4547dad32eb4ab4462c0293a4c071e63a3c10c02
zaafar <zaafar.tahir@gmail.com>
2017-01-23 05:39:42 -0500

    code cleanup added logic for area change added logic for enabled/disable plugin added logic for flasks changes
    Now PlayerFlasks list will always be updated
    and contain latest information about flasks
    
    TODO:
    flask action logic. Already thought about it, implementation left.

--- src/AutoFlaskManager/FlaskManagerCore.cs
+++ src/AutoFlaskManager/FlaskManagerCore.cs
@@ -1,15 +1,11 @@
 ﻿using PoeHUD.Controllers;
-using PoeHUD.Models;
 using PoeHUD.Poe.Components;
 using PoeHUD.Plugins;
-using System.IO;
 using System.Collections.Generic;
 using PoeHUD.Poe;
-using Newtonsoft.Json;
 using System.Windows.Forms;
 using System.Threading;
 using System;
-using System.Linq;
 using PoeHUD.Poe.EntityComponents;
 using PoeHUD.Poe.Elements;
 using System.Runtime.InteropServices;
@@ -18,258 +14,150 @@ namespace FlaskManager
 {
     public class FlaskManagerCore : BaseSettingsPlugin<FlaskManagerSettings>
     {
-        private const string FlaskEffectsFile = "FlaskEffects.txt";
-        private bool isThreadEnabled = false;
-        private FlasksConfig FlasksCfg;
-        //private bool getForegroundWindow;
-        //private static GameController Entity;
-        //private readonly Random random;
-        private long FlaskRootAddress = 0;
-        private IntPtr gameHandle;
+        private bool DEBUG = true;
+        private int logmsg_time = 3;
+        private int errmsg_time = 5;
+        private bool isThreadEnabled;
         private Element FlasksRoot;
-        private List<PlayerFlask> PlayerFlasks = new List<PlayerFlask>();
+        private IntPtr gameHandle;
+        private Entity playerInfo;
+        private List<PlayerFlask> PlayerFlasks;
 
+        public override void Initialise()
+        {
+            PlayerFlasks = new List<PlayerFlask>();
+            onFlaskManagerToggle();
+            GameController.Area.OnAreaChange += area => onAreaChange();
+            Settings.Enable.OnValueChanged +=  onFlaskManagerToggle;
+        }
         private void onFlaskManagerToggle()
         {
             try
             {
                 if (Settings.Enable.Value)
                 {
-                    LogMessage("Enabling FlaskManager.",1);
+                    if (DEBUG)
+                        LogMessage("Enabling FlaskManager.",logmsg_time);
+                    isThreadEnabled = true;
+                    PluginName = "Flask Manager";
+                    gameHandle = GameController.Window.Process.MainWindowHandle;
+                    ScanForFlaskAddress(GameController.Game.IngameState.UIRoot);
+                    playerInfo = GameController.Game.IngameState.Data.LocalPlayer;
+                    searchFlasksInventory();
                     //We are creating our plugin thread inside PoEHUD!
                     Thread flaskThread = new Thread(FlaskThread) { IsBackground = true };
-                    isThreadEnabled = true;
                     flaskThread.Start();
                 }
                 else
                 {
-                    LogMessage("Disabling FlaskManager.",1);
+                    if (DEBUG)
+                        LogMessage("Disabling FlaskManager.",logmsg_time);
+                    FlasksRoot = null;
+                    gameHandle = IntPtr.Zero;
+                    PlayerFlasks.Clear();
                     isThreadEnabled = false;
                 }
             }
             catch (Exception)
             {
 
-                LogError("Error Starting FlaskManager Thread.", 4);
+                LogError("Error Starting FlaskManager Thread.", errmsg_time);
             }
         }
-
-        public override void Initialise()
+        private void onAreaChange()
         {
-            PluginName = "Flask Manager";
-            ReadConfig(); //Reading the Flask Config File
-            gameHandle = GameController.Window.Process.MainWindowHandle;
-            onFlaskManagerToggle();
-            Settings.Enable.OnValueChanged += onFlaskManagerToggle;
-        }
-
-        private void FlaskMain()
-        {
-           //DebugFlasks();
-            ScanForFlask(GameController.Game.IngameState.UIRoot); //We are scanning for flasks that's on our inventory tab
-            InitPlayerFlasks(); //We are reading the Flask Config File etc
-            Entity localPlayer = GameController.Game.IngameState.Data.LocalPlayer; //Need LocalPlayer for detection of Health,Mana,CI and such
-            var isplayer = localPlayer.IsValid;
-            var life = localPlayer.GetComponent<Life>();
-            int hp = localPlayer.IsValid ? life.CurHP + life.CurES : 0;
-            //if(isplayer != false) LogMessage($"Our Current Health is {life.CurHP.ToString()} !!", 2); //Example code given, this is our own thread. 
-        }
-
-
-        public void UseFlasksForAction(FlaskAction action)
-        {
-            if (PlayerFlasks.Count == 0)
+            if (Settings.Enable.Value)
             {
-                ScanForFlask(GameController.Game.IngameState.UIRoot);
-                InitPlayerFlasks();
-
-                if (PlayerFlasks.Count == 0)
-                    
-                    LogMessage($"=================NO PLAYER FLASKS!!! NOT FIXED!!!!!!!!!! Addr: {FlaskRootAddress}===========================", 3);
-                else
-                    LogMessage("=================NO PLAYER FLASKS!!! FIXED!!!!!!!!!!===========================", 3);
+                ScanForFlaskAddress(GameController.Game.IngameState.UIRoot);
+                searchFlasksInventory();
             }
-
-            var fightFlasks = PlayerFlasks.Where(x => x.FlaskAction == action);
-
-            foreach (var flask in fightFlasks)
-            {
-                if (flask.FlaskCharges > flask.UseCharges && !IsFlaskInUse(flask))
-                {
-                    LogMessage($"Using flask in slot: {flask.Slot}", 2);
-                    UseFlask(flask);
-                }
-                else
-                {
-                    LogMessage($"Ignore flask in slot: {flask.Slot}", 3);
-                }
-            }
-            InitPlayerFlasks();
         }
-
-        private void InitPlayerFlasks()
+        private void searchFlasksInventory()
         {
+            if (DEBUG)
+                LogMessage("Searching for flasks in inventory.", logmsg_time);
             PlayerFlasks = new List<PlayerFlask>();
-
             if (FlasksRoot != null)
             {
-                foreach (var flaskElem in FlasksRoot.Children)
+                foreach (Element flaskElem in FlasksRoot.Children)
                 {
-                    var flaskInventItem = flaskElem.AsObject<InventoryItemIcon>();
-                    var item = flaskInventItem.Item;
+                    Entity item = flaskElem.AsObject<InventoryItemIcon>().Item;
                     if (item != null && item.HasComponent<Flask>())
                     {
                         var flaskCharges = item.GetComponent<Charges>();
-
-                        var newFlask = new PlayerFlask();
-                        newFlask.Slot = PlayerFlasks.Count;
-                        newFlask.FlaskCharges = flaskCharges.NumCharges;
-                        newFlask.UseCharges = flaskCharges.ChargesPerUse;
-                        newFlask.MaxCharges = flaskCharges.ChargesMax;
-  
-
-                        newFlask.Item = item;
-
-                        BaseItemType bit = GameController.Files.BaseItemTypes.Translate(item.Path);
-                        if (bit == null)
+                        var mods = item.GetComponent<Mods>();
+                        var flaskName = GameController.Files.BaseItemTypes.Translate(item.Path).BaseName;
+                        #region UniqueFlaskNotImplemented
+                        var isUnique = false;
+                        foreach (var mod in mods.ItemMods)
+                            if ( mod.RawName.ToLower().Contains("unique") )
+                                isUnique = true;
+                        if (isUnique)
+                        {
+                            LogError("Unique Flasks are not implemented.", errmsg_time);
                             continue;
-
-
-                        newFlask.DebugPosX = flaskElem.X;
-                        newFlask.FlaskName = bit.BaseName;
-
-                        var flaskCfg = GetConfigForFlask(newFlask.FlaskName);
-
-                        //This is my debug code.. I used it to test on certain flasks
-                        LogMessage($"{newFlask.FlaskName} !", 20);
-                        LogMessage($"{newFlask.FlaskCharges} !", 20);
-                        LogMessage($"{newFlask.UseCharges} !", 20);
-                        LogMessage($"{newFlask.MaxCharges} !", 20);
-                        LogMessage($"{newFlask.Slot} !", 20);
-
-                        if (flaskCfg != null)
-                        {
-                            newFlask.FlaskEffectName = flaskCfg.FlaskEffectName;
-                            newFlask.FlaskAction = flaskCfg.FlaskAction;
-                            newFlask.UseCharges = flaskCharges.ChargesPerUse;
-                            newFlask.MaxCharges = flaskCharges.ChargesMax;
-                            PlayerFlasks.Add(newFlask);
                         }
-                        else
-                        {
-                            LogMessage($"=========================================Can't find cfg for flask: {newFlask.FlaskName} !", 20);
-                        }
-
+                        #endregion
+                        PlayerFlask newFLask = new PlayerFlask();
+                        newFLask.FlaskName = flaskName;
+                        newFLask.Slot = PlayerFlasks.Count;
+                        newFLask.setSettings(Settings);
+                        newFLask.EnableDisableFlask();
+                        newFLask.Item = item;
+                        newFLask.MaxCharges = flaskCharges.ChargesMax;
+                        newFLask.UseCharges = flaskCharges.ChargesPerUse;
+                        newFLask.CurrentCharges = flaskCharges.NumCharges;
+                        newFLask.FlaskAction1 = FlaskAction.NONE;
+                        newFLask.FlaskAction2 = FlaskAction.NONE;
+                        PlayerFlasks.Add(newFLask);
                     }
                 }
             }
-
-            PlayerFlasks = PlayerFlasks.OrderBy(x => x.DebugPosX).ToList();
-
-            int slot = 1;
-            foreach (var flask in PlayerFlasks)
-            {
-                flask.Slot = slot;
-                slot++;
-            }
         }
-
-
-
-        private bool IsFlaskInUse(PlayerFlask flask)
+        private void ScanForFlaskAddress(Element elm)
         {
-            var life = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>();
-
-            foreach (var buff in life.Buffs)
-            {
-                if (buff.Name == flask.FlaskEffectName)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private void ScanForFlask(Element elm)
-        {
-            FlaskRootAddress = 0;
             foreach (var child in elm.Children)
             {
-                var flaskInventItem = child.AsObject<InventoryItemIcon>();
-                var item = flaskInventItem.Item;
+                var item = child.AsObject<InventoryItemIcon>().Item;
                 if (item != null)
                 {
                     if (item.HasComponent<Flask>())
                     {
                         FlasksRoot = child.Parent;
-                        FlaskRootAddress = elm.Address;
-                        LogMessage("================================Flasks Found!!=============================", 10);
+                        if (DEBUG)
+                            LogMessage("Found Flask Address.", logmsg_time);
                         return;
                     }
                 }
-
-                ScanForFlask(child);
-                if (FlaskRootAddress != 0)
-                    return;
+                ScanForFlaskAddress(child);
             }
         }
-
         public void UseFlask(PlayerFlask flask)
         {
             if (flask.Slot == 1)
-                UseFlaskByKey(Keys.D1);
+                KeyPressRelease(Keys.D1);
             else if (flask.Slot == 2)
-                UseFlaskByKey(Keys.D2);
+                KeyPressRelease(Keys.D2);
             else if (flask.Slot == 3)
-                UseFlaskByKey(Keys.D3);
+                KeyPressRelease(Keys.D3);
             else if (flask.Slot == 4)
-                UseFlaskByKey(Keys.D4);
+                KeyPressRelease(Keys.D4);
             else if (flask.Slot == 5)
-                UseFlaskByKey(Keys.D5);
+                KeyPressRelease(Keys.D5);
         }
-
-        private void UseFlaskByKey(Keys key)
+        private void FlaskMain()
         {
-            KeyPressRelease(key);
+            /*            var isplayer = localPlayer.IsValid;
+                        var life = localPlayer.GetComponent<Life>();
+                        int hp = localPlayer.IsValid ? life.CurHP + life.CurES : 0;
+                        //if(isplayer != false) LogMessage($"Our Current Health is {life.CurHP.ToString()} !!", 2); //Example code given, this is our own thread. 
+                        */
+            return;
         }
 
-
-
-        private string FlaskCFGPath => LocalPluginDirectory + @"\" + FlaskEffectsFile;
-
-        private void ReadConfig()
-        {
-            if (File.Exists(FlaskCFGPath))
-            {
-                string json = File.ReadAllText(FlaskCFGPath);
-                FlasksCfg = JsonConvert.DeserializeObject<FlasksConfig>(json);//, SettingsHub.jsonSettings
-            }
-            else
-            {
-                FlasksCfg = new FlasksConfig();
-
-
-                FlasksCfg.Flasks.Add(new FlaskConfig()
-                {
-                    FlaskName = "Quicksilver Flask",
-                    FlaskEffectName = "",
-                    //FlaskAction = FlaskAction.BeforeFight
-                });
-
-                using (var stream = new StreamWriter(File.Create(FlaskCFGPath)))
-                {
-                    string json = JsonConvert.SerializeObject(FlasksCfg, Formatting.Indented);//, SettingsHub.jsonSettings
-                    stream.Write(json);
-                }
-
-                LogMessage($"Can't find flask effects file! {FlaskCFGPath} !", 10);
-            }
-        }
         #region Keyboard Input
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-
         [return: MarshalAs(UnmanagedType.Bool)]
         [DllImport("user32.dll", SetLastError = true)]
         private static extern bool PostMessage(IntPtr hWnd, uint msg, UIntPtr wParam, UIntPtr lParam);
@@ -279,19 +167,16 @@ namespace FlaskManager
         {
             SendMessage(gameHandle, 0x100, (int)Key, 0);
         }
-
         public void KeyUp(Keys Key)
         {
             SendMessage(gameHandle, 0x101, (int)Key, 0);
         }
-
         public void KeyPressRelease(Keys key, int delay = 50)
         {
             KeyDown(key);
             Thread.Sleep(delay);
             KeyUp(key);
         }
-
         private void Write(string text, params object[] args)
         {
             foreach (var character in string.Format(text, args))
@@ -299,9 +184,7 @@ namespace FlaskManager
                 PostMessage(gameHandle, 0x0102, new UIntPtr(character), UIntPtr.Zero);
             }
         }
-
         #endregion
-
         #region Threading, do not touch
         private void FlaskThread()
         {
@@ -311,14 +194,9 @@ namespace FlaskManager
                     Thread.Sleep(1000);
             }
        }
-
         #endregion
-
-        private FlaskConfig GetConfigForFlask(string flaskName)
-        {
-            return FlasksCfg.Flasks.Find(x => x.FlaskName == flaskName);
-        }
     }
+
     public class AutoHPManaFlask
     {
 
@@ -349,39 +227,79 @@ namespace FlaskManager
 
     public class PlayerFlask
     {
+        public string FlaskName;
         public int Slot;
-        public string FlaskName;
-        public string FlaskEffectName;
-        public int FlaskCharges;
-        public FlaskAction FlaskAction;
-        public float DebugPosX;
+        public bool isEnabled;
+
         public Entity Item;
+        public FlaskAction FlaskAction1;
+        public FlaskAction FlaskAction2;
+
+        public int CurrentCharges;
         public int UseCharges;
         public int MaxCharges;
+
+        private FlaskManagerSettings Settings;
+        public void setSettings(FlaskManagerSettings s)
+        {
+            Settings = s;
+        }
+        public void EnableDisableFlask()
+        {
+            switch (Slot)
+            {
+                case 0:
+                    isEnabled = Settings.flaskSlot1Enable.Value;
+                    Settings.flaskSlot1Enable.OnValueChanged += EnableDisableFlask;
+                    break;
+                case 1:
+                    isEnabled = Settings.flaskSlot2Enable.Value;
+                    Settings.flaskSlot2Enable.OnValueChanged += EnableDisableFlask;
+                    break;
+                case 2:
+                    isEnabled = Settings.flaskSlot3Enable.Value;
+                    Settings.flaskSlot3Enable.OnValueChanged += EnableDisableFlask;
+                    break;
+                case 3:
+                    isEnabled = Settings.flaskSlot4Enable.Value;
+                    Settings.flaskSlot4Enable.OnValueChanged += EnableDisableFlask;
+                    break;
+                case 4:
+                    isEnabled = Settings.flaskSlot5Enable.Value;
+                    Settings.flaskSlot5Enable.OnValueChanged += EnableDisableFlask;
+                    break;
+                default:
+                    break;
+            }
+        }
+        ~PlayerFlask()
+        {
+            switch (Slot)
+            {
+                case 0:
+                    Settings.flaskSlot1Enable.OnValueChanged -= EnableDisableFlask;
+                    break;
+                case 1:
+                    Settings.flaskSlot2Enable.OnValueChanged -= EnableDisableFlask;
+                    break;
+                case 2:
+                    Settings.flaskSlot3Enable.OnValueChanged -= EnableDisableFlask;
+                    break;
+                case 3:
+                    Settings.flaskSlot4Enable.OnValueChanged -= EnableDisableFlask;
+                    break;
+                case 4:
+                    Settings.flaskSlot5Enable.OnValueChanged -= EnableDisableFlask;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-
-
-
-
-    public class FlasksConfig
-    {
-        public List<FlaskConfig> Flasks = new List<FlaskConfig>();
-    }
-
-
-    public class FlaskConfig
-    {
-        public string FlaskName;
-        public string FlaskEffectName;
-        public FlaskAction FlaskAction;
-        public int UseCharges;
-        public int MaxCharges;
-    }
-
-
     public enum FlaskAction : int
     {
-        LIFE = 0, //life, hybrid
+        NONE = 0,
+        LIFE, //life, hybrid
         MANA, //mana, hybrid
         DEFENSE, //bismuth, jade, stibnite, granite,
                  //amethyst, ruby, sapphire, topaz,
