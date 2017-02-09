327cda046e8d1a19c33a8eef714031c3590bd2be
zaafar <zaafar.tahir@gmail.com>
2017-02-09 13:06:34 -0500

    fixup menu

--- src/AutoFlaskManager/FlaskManagerSettings.cs
+++ src/AutoFlaskManager/FlaskManagerSettings.cs
@@ -36,14 +36,14 @@ namespace FlaskManager
             qSDur = new RangeNode<float>(1.5f, 0f, 10f);
             //Defensive Flask
             defFlaskEnable = false;
-            hPDefensive = new RangeNode<int>(50, 50, 100);
-            eSDefensive = new RangeNode<int>(50, 50, 100);
-            DefensiveDelay = new RangeNode<float>(3f, 2f, 6f);
+            hPDefensive = new RangeNode<int>(50, 0, 100);
+            eSDefensive = new RangeNode<int>(50, 0, 100);
+            DefensiveDelay = new RangeNode<float>(3f, 2f, 10f);
             //Offensive Flask
             offFlaskEnable = false;
-            hPOffensive = new RangeNode<int>(50, 50, 100);
-            eSOffensive = new RangeNode<int>(50, 50, 100);
-            OffensiveDelay = new RangeNode<float>(3f, 2f, 6f);
+            hPOffensive = new RangeNode<int>(50, 0, 100);
+            eSOffensive = new RangeNode<int>(50, 0, 100);
+            OffensiveDelay = new RangeNode<float>(3f, 2f, 10f);
             //Unique Flask
             uniqFlaskEnable = false;
             // Settings
@@ -70,76 +70,76 @@ namespace FlaskManager
         #endregion
 
         #region HP Mana Flask Menu
-        [Menu("HP/MANA % Auto Flask", 6)]
+        [Menu("HP/MANA % Auto Flask", 10)]
         public ToggleNode autoFlask { get; set; }
-        [Menu("Min Life % Auto HP Flask", 7, 6)]
+        [Menu("Min Life % Auto HP Flask", 11, 10)]
         public RangeNode<int> perHPFlask { get; set; }
-        [Menu("HP Flask Delay", 8, 6)]
+        [Menu("HP Flask Delay", 12, 10)]
         public RangeNode<float> HPDelay { get; set; }
-        [Menu("Min Mana % Auto Mana Flask", 9, 6)]
+        [Menu("Min Mana % Auto Mana Flask", 13, 10)]
         public RangeNode<float> PerManaFlask { get; set; }
-        [Menu("Mana Flask Delay", 10, 6)]
+        [Menu("Mana Flask Delay", 14, 10)]
         public RangeNode<float> ManaDelay { get; set; }
         #endregion
 
         #region Ailment Flask Menu
-        [Menu("Remove Ailment Flask", 11)]
+        [Menu("Remove Ailment Flask", 20)]
         public ToggleNode remAilment { get; set; }
-        [Menu("Remove Frozen Ailment", 12, 11)]
+        [Menu("Remove Frozen Ailment", 21, 20)]
         public ToggleNode remFrozen { get; set; }
-        [Menu("Remove Burning Ailment", 13, 11)]
+        [Menu("Remove Burning Ailment", 22, 20)]
         public ToggleNode remBurning { get; set; }
-        [Menu("Remove Shocked Ailment", 14, 11)]
+        [Menu("Remove Shocked Ailment", 23, 20)]
         public ToggleNode remShocked { get; set; }
-        [Menu("Remove Curse Ailment", 15, 11)]
+        [Menu("Remove Curse Ailment", 24, 20)]
         public ToggleNode remCurse { get; set; }
-        [Menu("Remove Poison Ailment", 16, 11)]
+        [Menu("Remove Poison Ailment", 25, 20)]
         public ToggleNode remPoison { get; set; }
-        [Menu("Remove Corrupting/Bleed Ailment", 17, 11)]
+        [Menu("Remove Corrupting/Bleed Ailment", 26, 20)]
         public ToggleNode remCorrupt { get; set; }
-        [Menu("Corrupting Blood Stacks", 18, 11)]
+        [Menu("Corrupting Blood Stacks", 27, 20)]
         public RangeNode<int> corrptCount { get; set; }
-        [Menu("Remove Ailment Post Duration (s)", 19, 11)]
+        [Menu("Remove Ailment Post Duration (s)", 28, 20)]
         public RangeNode<int> ailmentDur { get; set; } 
         #endregion
 
         #region Quick Sivler Flask Menu
-        [Menu("QuickSilver Flask", 20)]
+        [Menu("QuickSilver Flask", 30)]
         public ToggleNode qSEnable { get; set; }
-        [Menu("Use After Moving Post (s)", 21, 20)]
+        [Menu("Use After Moving Post (s)", 31, 30)]
         public RangeNode<float> qSDur { get; set; } 
         #endregion
 
         #region Defensive Flask Menu
-        [Menu("Defensive Flask", 23)]
+        [Menu("Defensive Flask", 40)]
         public ToggleNode defFlaskEnable { get; set; }
-        [Menu("Min Life % Auto Defensive Flask", 24, 23)]
+        [Menu("Min Life % Auto Defensive Flask", 41, 40)]
         public RangeNode<int> hPDefensive { get; set; }
-        [Menu("Min ES % Auto Defensive Flask", 25, 23)]
+        [Menu("Min ES % Auto Defensive Flask", 42, 40)]
         public RangeNode<int> eSDefensive { get; set; }
-        [Menu("Defensive Flask Delay", 10, 6)]
+        [Menu("Defensive Flask Delay", 43, 40)]
         public RangeNode<float> DefensiveDelay { get; set; }
         #endregion
 
         #region Offensive Flask Menu
-        [Menu("Offensive Flask", 28)]
+        [Menu("Offensive Flask", 50)]
         public ToggleNode offFlaskEnable { get; set; }
-        [Menu("Min Life % Auto Offensive Flask", 24, 23)]
+        [Menu("Min Life % Auto Offensive Flask", 51, 50)]
         public RangeNode<int> hPOffensive { get; set; }
-        [Menu("Min ES % Auto Offensive Flask", 25, 23)]
+        [Menu("Min ES % Auto Offensive Flask", 52, 50)]
         public RangeNode<int> eSOffensive { get; set; }
-        [Menu("Offensive Flask Delay", 10, 6)]
+        [Menu("Offensive Flask Delay", 53, 50)]
         public RangeNode<float> OffensiveDelay { get; set; }
         #endregion
 
         #region Unnique Flask Menu
-        [Menu("Unique Flask", 30)]
+        [Menu("Unique Flask", 60)]
         public ToggleNode uniqFlaskEnable { get; set; } 
         #endregion
 
         #region Settings Menu
-        [Menu("Flask Manager Settings", 100)]
-        public EmptyNode settingsHolder { get; set; }
+        [Menu("UI Settings/Debug Mode", 100)]
+        public EmptyNode uisettingsHolder { get; set; }
         [Menu("UI Enable", 101, 100)]
         public ToggleNode uiEnable { get; set; }
         [Menu("Position: X", 102, 100)]
@@ -148,18 +148,22 @@ namespace FlaskManager
         public RangeNode<float> positionY { get; set; }
         [Menu("Text Size", 104, 100)]
         public RangeNode<int> textSize { get; set; }
-        [Menu("Use Flask Slot 1", 105, 100)]
-        public ToggleNode flaskSlot1Enable { get; set; }
-        [Menu("Use Flask Slot 2", 106, 100)]
-        public ToggleNode flaskSlot2Enable { get; set; }
-        [Menu("Use Flask Slot 3", 107, 100)]
-        public ToggleNode flaskSlot3Enable { get; set; }
-        [Menu("Use Flask Slot 4", 108, 100)]
-        public ToggleNode flaskSlot4Enable { get; set; }
-        [Menu("Use Flask Slot 5", 109, 100)]
-        public ToggleNode flaskSlot5Enable { get; set; }
-        [Menu("Debug Mode", 110, 100)]
+        [Menu("Debug Mode", 105, 100)]
         public ToggleNode debugMode { get; set; }
+
+
+        [Menu("Enable/Disable Flasks", 110)]
+        public EmptyNode flasksettingsHolder { get; set; }
+        [Menu("Use Flask Slot 1", 111, 110)]
+        public ToggleNode flaskSlot1Enable { get; set; }
+        [Menu("Use Flask Slot 2", 112, 110)]
+        public ToggleNode flaskSlot2Enable { get; set; }
+        [Menu("Use Flask Slot 3", 113, 110)]
+        public ToggleNode flaskSlot3Enable { get; set; }
+        [Menu("Use Flask Slot 4", 114, 110)]
+        public ToggleNode flaskSlot4Enable { get; set; }
+        [Menu("Use Flask Slot 5", 115, 110)]
+        public ToggleNode flaskSlot5Enable { get; set; }
         #endregion
     }
 }
