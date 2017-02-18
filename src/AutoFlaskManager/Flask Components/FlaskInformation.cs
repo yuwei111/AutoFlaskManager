d1ed558abf5d0cd7f392a0570b81308b8786c0d7
zaafar <zaafar.tahir@gmail.com>
2017-02-18 04:17:34 -0500

    reverted commit.

--- src/AutoFlaskManager/Flask Components/FlaskInformation.cs
+++ src/AutoFlaskManager/Flask Components/FlaskInformation.cs
@@ -7,8 +7,5 @@ namespace FlaskManager
         public Dictionary<string, FlaskAction> UniqueFlaskNames { get; set; }
         public Dictionary<string, FlaskAction> FlaskTypes { get; set; }
         public Dictionary<string, FlaskAction> FlaskMods { get; set; }
-        public Dictionary<string, FlaskAction> Ignore_Pattern { get; set; }
-        public Dictionary<string, FlaskAction> Offensive_Pattern { get; set; }
-        public Dictionary<string, FlaskAction> Defensive_Pattern { get; set; }
     }
 }
